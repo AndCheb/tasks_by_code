e71a3ad54b90253877933914480fc73a7439f51f
Andrey Cheban <anceb@yandex.ru>
2023-06-17 20:47:22 +0300

    add task 11

---
+++ Задача 011/task11.php
@@ -0,0 +1,12 @@
+<?php
+
+$K = 5; // оценка
+
+switch ($K) {
+  case 1: echo 'плохо'; break;
+  case 2: echo 'неудовлетворительно'; break;
+  case 3: echo 'удовлетворительно'; break;
+  case 4: echo 'хорошо'; break;
+  case 5: echo 'отлично'; break;
+  default: echo 'ошибка';
+}
