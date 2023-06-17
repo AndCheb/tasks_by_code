6054271bfca49d3e2a3fb37c61c1f29f34db4477
Andrey Cheban <anceb@yandex.ru>
2023-06-17 11:18:22 +0300

    add task 6

---
+++ Задача 6/task6.js
@@ -0,0 +1,5 @@
+let n = 10;
+console.log(n > 0 ? ++n : n);
+
+// Функция проверки условия
+const positiveAddOne = a => console.log(a > 0 ? ++a : a)
---
+++ Задача 6/task6.php
@@ -0,0 +1,12 @@
+<?php
+
+$n = 10;
+echo $n > 0 ? ++$n : $n;
+
+// Функция проверки условия
+function positive_add_one($a)
+{
+  echo $a > 0 ? ++$a : $a;
+}
+
+?>
