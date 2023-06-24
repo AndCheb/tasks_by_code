728a541c09fe13783d1595619e60c8c0883a2be2
Andrey Cheban <anceb@yandex.ru>
2023-06-24 12:27:44 +0300

    add task 18

---
+++ Задача 018/task18.js
@@ -0,0 +1,4 @@
+const A = 11;
+const result = A % 2 === 0;
+
+const isEven = x => x % 2 === 0;
---
+++ Задача 018/task18.php
@@ -0,0 +1,9 @@
+<?php
+
+$a = 20;
+$result = $a % 2 === 0 ? 'true' : 'false';
+
+function is_even($x)
+{
+	return $x % 2 === 0 ? 'true' : 'false';
+}
