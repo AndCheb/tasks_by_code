5e59baf9f958071f1858e9d057630798536c9cc3
Andrey Cheban <anceb@yandex.ru>
2023-06-25 10:55:11 +0300

    add task 19

---
+++ Задача 019/task19.js
@@ -0,0 +1,5 @@
+const a = [4, 1, 3, 4, 6, 2, 5, 9, 8, 11];
+
+const result = a.reduce((total, item) => total + item, 0);
+
+const sumOfNumbers = arr => arr.reduce((total, item) => total + item, 0);
---
+++ Задача 019/task19.php
@@ -0,0 +1,10 @@
+<?php
+
+$a = [3, 2, 4, 7, 1, 5, 9, 8, 6, 10];
+
+$result = array_sum($a);
+
+function sum_of_numbers($arr)
+{
+  echo array_sum($arr);
+}
