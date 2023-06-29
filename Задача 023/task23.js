a2e649035acef4c6d338500b40c5c3677368d678
Andrey Cheban <anceb@yandex.ru>
2023-06-29 12:18:21 +0300

    add task 23

---
+++ Задача 023/task23.js
@@ -0,0 +1,3 @@
+const cubeVolume = a => Math.pow(a, 3);
+
+const cubeSurfaceArea = a => 6 * Math.pow(a, 2);
---
+++ Задача 023/task23.php
@@ -0,0 +1,11 @@
+<?php
+
+function cube_volume($a)
+{
+  return pow($a, 3);
+}
+
+function cube_surface_area($a)
+{
+  return 6 * pow($a, 2);
+}
