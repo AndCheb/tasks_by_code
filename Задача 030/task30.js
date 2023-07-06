e1389c5250bb8a1f31663c0c1090daca42afab67
Andrey Cheban <anceb@yandex.ru>
2023-07-06 16:02:52 +0300

    add task 30

---
+++ Задача 030/task30.js
@@ -0,0 +1,9 @@
+const x = 2;
+const y = -1;
+const z = 0;
+
+let result = 0;
+
+x > 0 && result++;
+y > 0 && result++;
+z > 0 && result++;
---
+++ Задача 030/task30.php
@@ -0,0 +1,11 @@
+<?php
+
+$a = 0;
+$b = 1;
+$c = 2;
+
+$result = 0;
+
+$a > 0 && $result++;
+$b > 0 && $result++;
+$c > 0 && $result++;
