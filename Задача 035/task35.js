07a8d115e3d2a205a57fbb1809945c7442f7005c
Andrey Cheban <anceb@yandex.ru>
2023-07-11 11:48:31 +0300

    add task 35

---
+++ Задача 035/task35.js
@@ -0,0 +1,5 @@
+const kilogramOfSweets = 89;
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`${i} kg of sweets cost ${89 * i}`);
+}
---
+++ Задача 035/task35.php
@@ -0,0 +1,8 @@
+<?php
+
+$kg_sweets = 65;
+
+for ($i = 1; $i <= 10; $i++) 
+{
+  echo "$i kg of sweets cost " . $i * 65 . "\n";
+}
