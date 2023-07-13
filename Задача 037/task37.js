ead1cd7f7eab4af4c5ee0b293fc14171d910446b
Andrey Cheban <anceb@yandex.ru>
2023-07-13 12:40:20 +0300

    add task 37

---
+++ Задача 037/task37.js
@@ -0,0 +1,4 @@
+const kg = 24;
+
+for (let i = 1.2; i <= 2; i += 0.2)
+  console.log((kg * i).toFixed(2));
---
+++ Задача 037/task37.php
@@ -0,0 +1,6 @@
+<?php
+
+$kg = 33;
+
+for ($i = 1.2; $i <= 2; $i += 0.2)
+  echo number_format($kg * $i, 2) . "\n";
