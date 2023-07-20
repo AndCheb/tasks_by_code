cad194cfd813feb2f8f89c64c97810572a1a3e6d
Andrey Cheban <anceb@yandex.ru>
2023-07-20 12:16:55 +0300

    add task 44

---
+++ Задача 044/task44.js
@@ -0,0 +1,7 @@
+﻿const x = 4;
+const y = 3;
+
+const sum = Math.pow(x, 2) + Math.pow(y, 2);
+const difference = Math.pow(x, 2) - Math.pow(y, 2);
+const multiplication = Math.pow(x, 2) * Math.pow(y, 2);
+const quotient = Math.pow(x, 2) / Math.pow(y, 2);
---
+++ Задача 044/task44.php
@@ -0,0 +1,9 @@
+﻿<?php
+
+$x = 6;
+$y = 3;
+
+$sum = pow($x, 2) + pow($y, 2);
+$difference = pow($x, 2) - pow($y, 2);
+$multiplication = pow($x, 2) * pow($y, 2);
+$quotient = pow($x, 2) / pow($y, 2);
