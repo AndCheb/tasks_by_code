d20521c452f1b8a8b4c257ee2a5e6ceb771fdab6
Andrey Cheban <anceb@yandex.ru>
2023-07-22 15:57:54 +0300

    add task 46

---
+++ Задачи/Задача 046/task46.js
@@ -0,0 +1,4 @@
+﻿const x = 45;
+
+const sum = x.toString().split('').reduce((total, value) => total += +value, 0);
+const multipl = x.toString().split('').reduce((total, value) => total *= +value, 1);
---
+++ Задачи/Задача 046/task46.php
@@ -0,0 +1,8 @@
+﻿<?php
+
+$x = 39;
+
+$x_parts = str_split(strval($x));
+
+$sum = $x_parts[0] + $x_parts[1];
+$multipl = $x_parts[0] * $x_parts[1];
