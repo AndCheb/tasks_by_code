e443109824d51692f5d88076fe07aace9f7a60b7
Andrey Cheban <anceb@yandex.ru>
2023-07-23 12:07:07 +0300

    add task 47

---
+++ Задачи/Задача 047/task47.js
@@ -0,0 +1,3 @@
+﻿const number = 54;
+
+const result = +number.toString().split('').reverse().join('');
---
+++ Задачи/Задача 047/task47.php
@@ -0,0 +1,7 @@
+﻿<?php
+
+$x = 92;
+
+$x_parts = array_reverse(str_split(strval($x)));
+
+$result = +($x_parts[0] . $x_parts[1]);
