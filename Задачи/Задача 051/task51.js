49c935ab07626d72965851c84209befc9fbc01e1
Andrey Cheban <anceb@yandex.ru>
2023-07-27 15:29:07 +0300

    add task 51

---
+++ Задачи/Задача 051/task51.js
@@ -0,0 +1,10 @@
+﻿const a = 4;
+const b = 11;
+
+const listOfNumbers = [];
+
+for (let i = b - 1; i > a; i--) {
+	listOfNumbers.push(i);
+}
+
+console.dir({ listOfNumbers, 'amount of numbers': listOfNumbers.length });
---
+++ Задачи/Задача 051/task51.php
@@ -0,0 +1,14 @@
+﻿<?php
+
+$a = 2;
+$b = 6;
+
+$list_of_numbers = [];
+
+while($b > $a + 1) 
+{
+	 array_push($list_of_numbers, --$b);
+	 echo "$b\n";
+}
+
+echo 'amount of numbers: ' . count($list_of_numbers);
