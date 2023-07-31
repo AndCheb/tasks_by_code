6b42883b97c9a51778b53b1f5145dbb841316eec
Andrey Cheban <anceb@yandex.ru>
2023-07-31 12:33:17 +0300

    add task 55

---
+++ Задачи/Задача 055/task55.js
@@ -0,0 +1,7 @@
+﻿const nums = [4, 5, 7, 8, 9, 11, 14, 16, 22, 28];
+
+const isSquare = k => {
+  const o = {};
+  k.forEach(i => o[i] = Number.isInteger(Math.sqrt(i)));
+  return o;
+};
---
+++ Задачи/Задача 055/task55.php
@@ -0,0 +1,16 @@
+﻿<?php
+
+$nums = [25, 33, 49, 51, 55, 64, 76, 89, 95, 101];
+
+function is_square($arr)
+{
+  $result = 0;
+  foreach ($arr as $num)
+  {
+    if (((int)sqrt($num) * (int)sqrt($num)) === $num)
+    {
+      $result++;
+    }
+  }
+  return $result;
+}
