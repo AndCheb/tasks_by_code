74abb20d3da3fb9f4cbfc6f269857f9181bbc064
Andrey Cheban <anceb@yandex.ru>
2023-08-05 13:53:50 +0300

    add task 60

---
+++ Задачи/Задача 060/task60.js
@@ -0,0 +1,7 @@
+const a = 3;
+const b = 9;
+
+let result = 0;
+
+for (let i = a; i <= b; i++)
+  result += Math.pow(i, 2);
---
+++ Задачи/Задача 060/task60.php
@@ -0,0 +1,9 @@
+<?php
+
+$a = 3;
+$b = 14;
+
+$result = 0;
+
+for ($i = $a; $i <= $b; $i++)
+  $result += pow($i, 2);
