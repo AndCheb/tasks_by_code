708ad36cff65787d6072ea67a754312991d825df
Andrey Cheban <anceb@yandex.ru>
2023-08-17 12:19:59 +0300

    add task 72

---
+++ Задачи/Задача 072/task72.js
@@ -0,0 +1,6 @@
+const n = 13;
+const result = [];
+
+for (let i = 1; i <= n; i += 2) {
+  result.push(i);
+}
---
+++ Задачи/Задача 072/task72.php
@@ -0,0 +1,9 @@
+<?php
+
+$n = 8;
+$result = [];
+
+for ($i = 1; $i <= $n; $i += 2)
+{
+  array_push($result, $i);
+}
