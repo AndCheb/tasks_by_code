a693f3d1dbb025a709c4324e7f69b7033518e88f
Andrey Cheban <anceb@yandex.ru>
2023-08-19 13:25:42 +0300

    add task 74

---
+++ Задачи/Задача 074/task74.js
@@ -0,0 +1,18 @@
+const rectangles = [
+  { a: 10, b: 12 },
+  { a: 11, b: 19 },
+  { a: 13, b: 15 },
+  { a: 32, b: 13 },
+  { a: 10, b: 19 },
+  { a: 11, b: 12 },
+  { a: 22, b: 21 },
+  { a: 40, b: 12 },
+];
+
+const area = [];
+
+rectangles.forEach(item => {
+  area.push(item.a * item.b);
+});
+
+const result = rectangles[area.findIndex(i => i === Math.min(...area))];
---
+++ Задачи/Задача 074/task74.php
@@ -0,0 +1,22 @@
+<?php
+
+$rectangles =
+[
+  [3, 5],
+  [2, 3],
+  [2, 6],
+  [4, 3]
+];
+
+$area = [];
+
+foreach ($rectangles as $values)
+{
+  array_push($area, $values[0] * $values[1]);
+};
+
+$result = 
+[
+  $rectangles[array_search(min($area), $area)][0],
+  $rectangles[array_search(min($area), $area)][1]
+];
