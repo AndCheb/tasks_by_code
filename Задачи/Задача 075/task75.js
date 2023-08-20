5fd380208619f559c41c059f49382eec9effd493
Andrey Cheban <anceb@yandex.ru>
2023-08-20 13:47:07 +0300

    add task 75

---
+++ Задачи/Задача 075/task75.js
@@ -0,0 +1,1 @@
+const powersOfTwo = n => Array.from({ length: n }, (_, i) => 2 ** i);
---
+++ Задачи/Задача 075/task75.php
@@ -0,0 +1,10 @@
+<?php
+
+$n = 5;
+
+$result = [];
+
+for ($i = 1; $i <= $n; $i++)
+{
+  array_push($result, pow(2, $i));
+}
